b86b78896ed8b084df1d51bdf8f83de6d040625f
Yonas Alamneh <yonasbiochem@gmail.comgit>
2019-02-21 23:33:49 -0500

    First Lab work

---
+++ main.js
@@ -0,0 +1,6 @@
+let yourName = prompt('what is the Beautiful color')
+let favoriteMovie = prompt('what is your Movie')
+let favoritefood = prompt('What is your food') 
+confirm('${yourName} your favorite Movie is ${favoriteMovie} and you love ${favoritefood}')
+
+console.log('${yourName} your favorite movie is ${favoriteMove} and you love ${favoritefood}')
